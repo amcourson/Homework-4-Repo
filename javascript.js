10d8e3c40e79ac9b55d13635090ccaf95fa25316
amcourson <amcourson09@sbcglobal.net>
2021-01-15 20:46:06 -0600

    adding quiz questions

--- javascript.js
+++ javascript.js
@@ -1,18 +1,57 @@
-var timeRemaining = document.getElementById("time")
+var startButton = document.querySelector(".start-button");
+var timeRemaining = document.getElementById(".time")
 
+var secondsLeft =50;
 
-
-
-function timeLeft(){
-    var timeRemaining = 50;
-
-    var timerInterval = setInterval(function() {
-        timeRemaining.textContent = timeRemaining + "seconds remaining";
-        timeRemaining--;
-
-        if (timeRemaining === 0){
-            timeRemaining.textContent = "";
+function setTime(){
+    var timerInterval = setInterval(function(){
+        secondsLeft--;
+        timeRemaining.textContent = secondsLeft + "50 seconds left";
+        if (secondsLeft ===0) {
             clearInterval(timerInterval);
+            sendMessage();
         }
     },1000);
 }
+
+function reduceTime(){
+    timerInterval.textContent = "";
+   //if chosen answer is wrong take off time
+   //if chose answer is correct continue
+}
+
+
+//event listener button
+// when button is clicked, if wrong, take off time and move to new question. 
+//if correct move to new question
+//at the end display results 
+//ask initials and display highscores 
+
+function generateQuiz
+
+function showQuestions(){
+    var myQuestions = [
+        {
+            question: "What is an array?",
+            answers: {
+                a:'An object that allows you to store multiple variable in a single varriable.',
+                b:'Something used in JavaScript.',
+                c: 'A list',
+            },
+            correctAnwser: 'a'
+        },
+    {
+        question: "What is a dataset?",
+        answers: {
+            a:'',
+            b:'',
+            c:'',
+        },
+        correctAnswer: ''
+    }
+}
+    ];
+
+function showResults
+
+function displayHighScores
